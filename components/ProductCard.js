f0be2b29bf305eab686ea8526817030f2eb03a70
jaxonhill <jaxoncharleshill@gmail.com>
2023-01-17 23:52:58 -0800

    Add images to homepage, rough styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ProductCard({ product }) {
     return (
-        <div key={product.id}>
+        <div className="w-full bg-white rounded-2xl shadow p-4" key={product.id}>
+            <Image src={product.imgPath} width={400} height={400} />
             <Link className="text-blue-400 hover:underline" href={`/products/${encodeURIComponent(product.slug)}`}>
                 {product.slug}
             </Link>
