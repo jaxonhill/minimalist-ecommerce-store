14d5521e39936e8eb7a05bf62274b12164c7872d
jaxonhill <jaxoncharleshill@gmail.com>
2023-01-27 16:20:20 -0800

    Add increasing or decreasing quantity on cart page

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,6 +2,29 @@ import Link from "next/link"
 import Image from "next/image"
 
 export default function CheckoutPage({ cartItems, setCartItems }) {
+    const handleDecrease = (item) => {
+
+        // If the quantity is less than or equal to 1, 
+        // then just setCartItems to every item except this one
+        if (item.quantity <= 1) {
+            const newItems = cartItems.filter((eachItem) => {
+                return eachItem.slug !== item.slug;
+            })
+            setCartItems(newItems);
+            return;
+        }
+
+        // If here, the quantity of the item is greater than 1,
+        // so its possible to delete quantity by 1 safely for the targetted item
+        const newItems = cartItems.map((eachItem) => {
+            if (item.slug === eachItem.slug) {
+                return { ...item, quantity: item.quantity - 1 };
+            }
+            return eachItem;
+        })
+        setCartItems(newItems);
+    }
+
     return (
         cartItems.length <= 0
             ?
@@ -16,26 +39,30 @@ export default function CheckoutPage({ cartItems, setCartItems }) {
                 <ul className="flex flex-col gap-6">
                     {cartItems.map((item) => {
                         return (
-                            <article className="p-4 bg-white shadow rounded-2xl">
-                                {/* <div className="w-full flex justify-between gap-4"> */}
-                                {/* <div className="aspect-square relative overflow-hidden rounded-2xl w-1/3">
-                                        <Image src={item.imgPath} alt={item.title} className="object-contain" fill sizes="(max-width: 800px)" priority />
-                                    </div> */}
+                            <article key={item.slug} className="p-4 bg-white shadow rounded-2xl">
                                 <div className="flex flex-col gap-1 pb-3 border-b border-b-gray-200 mb-3">
                                     <h1 className="font-bold truncate text-gray-600">{item.title}</h1>
-                                    {/* </div> */}
                                 </div>
                                 <div className="flex justify-between">
-                                    {/* <p className="font-bold text-gray-600">Quantity:</p> */}
                                     <div className="flex justify-between gap-2">
                                         <p>${item.price}</p>
                                         <p>&bull;</p>
                                         <p>{item.size}</p>
                                     </div>
                                     <div className="flex gap-3">
-                                        <button className="px-1">-</button>
+                                        <button
+                                            className="px-1"
+                                            onClick={() => handleDecrease(item)}
+                                        >
+                                            -
+                                        </button>
                                         <p>{item.quantity}</p>
-                                        <button className="px-1">+</button>
+                                        <button
+                                            className="px-1"
+                                            onClick={() => setCartItems([...cartItems, { ...item, quantity: item.quantity + 1 }])}
+                                        >
+                                            +
+                                        </button>
                                     </div>
                                 </div>
                             </article>
