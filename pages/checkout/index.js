8177c73b2af583e2ed6f1439a8d556e7c11de6fe
jaxonhill <jaxoncharleshill@gmail.com>
2023-01-28 13:13:34 -0800

    Add checkout page animations

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,8 +1,13 @@
 import Link from "next/link"
+import { motion } from "framer-motion"
 
 export default function CheckoutScreen() {
     return (
-        <div className="flex flex-col md:items-center md:pt-12">
+        <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="flex flex-col md:items-center md:pt-12"
+        >
             <h1 className="font-bold mb-4 text-4xl md:text-5xl">Checkout</h1>
             <p className="max-w-4xl text-gray-600 text-xl mb-8 leading-relaxed md:leading-snug md:text-3xl md:text-center md:mb-12">
                 If this were a real eCommerce store with a real domain,
@@ -11,6 +16,6 @@ export default function CheckoutScreen() {
             <Link href="/products">
                 <button className="rounded-2xl py-4 px-16 bg-gray-800 text-white text-2xl hover:bg-gray-700">Products</button>
             </Link>
-        </div>
+        </motion.div>
     )
 }
